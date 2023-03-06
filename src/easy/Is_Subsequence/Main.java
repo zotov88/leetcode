dcc3357560e539f002a98248aa7566d9ec21e209
blountam <blountam@s21-student.21-school.ru>
2023-03-07 00:33:24 +0300

    commit

---
+++ src/easy/Is_Subsequence/Main.java
@@ -0,0 +1,19 @@
+package easy.Is_Subsequence;
+
+public class Main {
+    public static void main(String[] args) {
+        String s1 = "axc";
+        String s2 = "ahbgdc";
+        System.out.println(isSubsequence(s1, s2));
+    }
+
+    private static boolean isSubsequence(String s1, String s2) {
+        int i = 0;
+        for (int j = 0; i < s1.length() && j < s2.length(); j++) {
+            if (s1.charAt(i) == s2.charAt(j)) {
+                i++;
+            }
+        }
+        return i == s1.length();
+    }
+}
---
+++ src/medium/Container_With_Most_Water/Main.java
@@ -0,0 +1,50 @@
+package medium.Container_With_Most_Water;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] height = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+        System.out.println(maxArea(height));
+    }
+
+    private static int maxArea(int[] height) {
+        int maxArea = 0;
+        int left = 0;
+        int right = height.length - 1;
+        while (left < right) {
+            int tmpArea = Math.min(height[left], height[right]) * (right - left);
+            if (tmpArea > maxArea) {
+                maxArea = tmpArea;
+            }
+            if (height[left] < height[right]) {
+                left++;
+            } else if (height[left] > height[right]) {
+                right--;
+            } else {
+                left++;
+                right--;
+            }
+        }
+        return maxArea;
+    }
+
+    private static int maxArea1(int[] height) {
+        int maxArea = 0;
+        int previousHeightLeftBoard = 0;
+        for (int i = 0; i < height.length - 1; i++) {
+            if (height[i] <= previousHeightLeftBoard) {
+                continue;
+            }
+            for (int j = height.length - 1; j > i ; j--) {
+                int tmpArea = Math.min(height[i], height[j]) * (j - i);
+                if (tmpArea > maxArea) {
+                    maxArea = tmpArea;
+                }
+                if (height[j] >= height[i]) {
+                    break;
+                }
+            }
+            previousHeightLeftBoard = height[i];
+        }
+        return maxArea;
+    }
+}

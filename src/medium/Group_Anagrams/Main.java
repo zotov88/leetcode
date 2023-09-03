c8153899984c2be643674d281adff05ddc182a29
zotov88 <zotov_l88@mail.ru>
2023-09-03 17:51:09 +0300

    49

--- src/medium/Group_Anagrams/Main.java
+++ src/medium/Group_Anagrams/Main.java
@@ -4,31 +4,21 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        String[] strs = {"eat","tea","tan","ate","nat","bat"};
+        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
         System.out.println(groupAnagrams(strs));
     }
 
     public static List<List<String>> groupAnagrams(String[] strs) {
         Map<String, List<String>> map = new HashMap<>();
-
         for (String str : strs) {
             char[] array = str.toCharArray();
             Arrays.sort(array);
             String key = Arrays.toString(array);
-            List<String> list;
-            if (map.containsKey(key)) {
-                list = map.get(key);
-            } else {
-                list = new ArrayList<>();
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
             }
-            list.add(str);
-            map.put(key, list);
+            map.get(key).add(str);
         }
-
-        List<List<String>> result = new ArrayList<>();
-        for (String s : map.keySet()) {
-            result.add(map.get(s));
-        }
-        return result;
+        return new ArrayList<>(map.values());
     }
 }

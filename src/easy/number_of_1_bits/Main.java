e96fdf67181d6e9b0455c5ee6c5288528ddf39fb
zotov88 <zotov_l88@mail.ru>
2023-11-29 16:59:08 +0300

    191

---
+++ src/easy/number_of_1_bits/Main.java
@@ -0,0 +1,21 @@
+package easy.number_of_1_bits;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(hammingWeight(000000000000000000000100001011));
+    }
+
+    public static int hammingWeight(int n) {
+        int count = 0;
+
+        for (int i = 0; i < 32; i++) {
+            count = (n & 1) == 1 ? count + 1 : count;
+            n = n >> 1;
+        }
+
+        return count;
+    }
+
+}

ace8acd50c8030ff08b283d9c35d0c6bc401c5ca
zotov88 <zotov_l88@mail.ru>
2023-12-04 01:47:55 +0300

    283

---
+++ src/easy/move_zeroes/Main.java
@@ -0,0 +1,27 @@
+package easy.move_zeroes;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{0, 1, 0, 4, 0, 2, 0, 9, 0, 6};
+        moveZeroes(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    public static void moveZeroes(int[] nums) {
+        int index = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[index++] = nums[i];
+            }
+        }
+
+        for (int i = index; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+    }
+
+}

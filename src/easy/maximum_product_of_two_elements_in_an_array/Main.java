fcd94f62d9bfc23407425ec130832f48096a487a
zotov88 <zotov_l88@mail.ru>
2023-12-12 15:27:21 +0300

    1464

---
+++ src/easy/maximum_product_of_two_elements_in_an_array/Main.java
@@ -0,0 +1,24 @@
+package easy.maximum_product_of_two_elements_in_an_array;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{3, 7, 4};
+        System.out.println(maxProduct(nums));
+    }
+
+    public static int maxProduct(int[] nums) {
+        int max = Math.max(nums[0], nums[1]);
+        int nextMax = Math.min(nums[0], nums[1]);
+
+        for (int i = 2; i < nums.length; i++) {
+            if (nums[i] >= max) {
+                nextMax = max;
+                max = nums[i];
+            } else if (nums[i] > nextMax) {
+                nextMax = nums[i];
+            }
+        }
+        return (max - 1) * (nextMax - 1);
+    }
+}

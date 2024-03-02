146092bb4a7603d9d506ba1e3b63001c1c648428
zotov88 <zotov_l88@mail.ru>
2024-03-02 20:35:05 +0300

    977

--- src/javasolution/easy/squares_of_a_sorted_array/Main.java
+++ src/javasolution/easy/squares_of_a_sorted_array/Main.java
@@ -1,6 +1,8 @@
 package javasolution.easy.squares_of_a_sorted_array;
 
 import java.util.Arrays;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,6 +11,33 @@ public class Main {
     }
 
     public static int[] sortedSquares(int[] nums) {
+        return Arrays.stream(nums)
+                .mapToObj(num -> new Box(num * num))
+                .collect(Collectors.toCollection(TreeSet::new))
+                .stream()
+                .mapToInt(Box::getI)
+                .toArray();
+    }
+
+    private static class Box implements Comparable<Box> {
+
+        int i;
+
+        public Box(int i) {
+            this.i = i;
+        }
+
+        public int getI() {
+            return i;
+        }
+
+        @Override
+        public int compareTo(Box o) {
+            return  (o.getI() > getI()) ? -1 : 1;
+        }
+    }
+
+    public static int[] sortedSquares2(int[] nums) {
         int x = 0;
 
         for (int i = nums.length - 1; i > 0; i--) {

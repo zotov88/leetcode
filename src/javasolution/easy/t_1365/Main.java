601500d4598eaf65682417e7c158522c5ee4f23e
zotov88 <zotov_l88@mail.ru>
2024-04-18 16:05:51 +0300

    1365

---
+++ src/javasolution/easy/t_1365/Main.java
@@ -0,0 +1,33 @@
+package javasolution.easy.t_1365;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(smallerNumbersThanCurrent(new int[]{6, 5, 4, 8})));
+    }
+
+    public static int[] smallerNumbersThanCurrent(int[] nums) {
+        int max = Arrays.stream(nums).max().getAsInt();
+        int[] indexes = new int[max + 1];
+
+        Arrays.stream(nums).forEach(i -> indexes[i]++);
+
+        int[] result = new int[nums.length];
+
+        for (int i = 0; i < result.length; i++) {
+            result[i] = getSumIntervalArray(indexes, nums[i]);
+        }
+
+        return result;
+    }
+
+    private static Integer getSumIntervalArray(int[] indexes, int num) {
+        if (num == 0) {
+            return 0;
+        }
+        int[] tmp = Arrays.copyOfRange(indexes, 0, num);
+
+        return Arrays.stream(tmp).sum();
+    }
+}
